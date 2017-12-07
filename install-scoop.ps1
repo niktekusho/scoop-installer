79be9250d6722b749e9806832ed20f49a445a6c2
Nicola <nicola.dalmaso@outlook.com>
2017-12-07 14:40:49 +0100

    Added PowerShell version check

--- install-scoop.ps1
+++ install-scoop.ps1
@@ -1,5 +1,16 @@
-Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+$PowerShellVersion = (Get-Host).Version
 
-iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+function Install
+{
+  Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+
+  iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+}
+
+If ($PowerShellVersion.Major -gt 3) {
+  Install
+} Else {
+  Read-Host "You have an older version of PowerShell. Scoop requires at least PowerShell 3."
+}
 
 Read-Host "Press Enter to exit"
